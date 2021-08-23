13cd4afd33e80b4b6cb288388fa27970fd9e0fc0
Cristian Montes <cmontesmujica@gmail.com>
2021-08-22 21:26:41 -0700

    working on the post end point

--- server.js
+++ server.js
@@ -1,12 +1,39 @@
 const express = require('express');
 
 const app = express();
+app.listen(3000);
+
+const dataBase = {
+    users:[
+        {
+            id: '123',
+            name: 'Steven',
+            email: 'steven@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sallyn@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+   
 
 app.get('/', (req, res) =>{
     res.send('this is working my friend');
 } );
 
-app.listen(3000);
+app.post('/signing', (req, res) => {
+    res.json('PLEASE SIGN IN MY FRIEND');
+})
+
+
 
 /*
 
