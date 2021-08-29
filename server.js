52fd43884bc1f695d76f7067ea335b9ee9d24ce0
Cristian Montes <cmontesmujica@gmail.com>
2021-08-28 18:52:12 -0700

    minor changes added

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-app.listen(3000);
+
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const cors = require('cors');
 
 app.use(bodyParser.json());
+app.use(cors());
 const dataBase = {
     users:[
         {
@@ -30,7 +33,7 @@ app.get('/', (req, res) =>{
     res.send(dataBase.users);
 } );
 
-app.post('/signing', (req, res) => {
+app.post('/signin', (req, res) => {
     if(req.body.email === dataBase.users[0].email && req.body.password === dataBase.users[0].password){
         res.json('success');
     } else {
@@ -45,9 +48,42 @@ app.post('/register', (req, res) => {
         id: '125',
         name: name,
         email: email,
-        password: password,
         entries: 0,
         joined: new Date()
      })
      res.json(dataBase.users[dataBase.users.length-1]);
 })
+
+app.get('/profile/:id', (req, res) => {
+    const {id} = req.params;
+    let found = false;
+    dataBase.users.forEach( user => {
+        if(user.id === id){
+            found = true;
+            return res.json(user);
+        }
+    })
+    if(!found){
+        res.status(400).json('not found');
+    }
+})
+
+app.put('/image', (req, res) => {
+    const {id} = req.body;
+    let found = false;
+    dataBase.users.forEach( user => {
+        if(user.id === id){
+            found = true;
+            user.entries++
+            return res.json(user.entries);
+        }
+    })
+    if(!found){
+        res.status(400).json('not found');
+    }
+})
+
+
+app.listen(3000, ()=> {
+    console.log('app is running on port 3000');
+});
