5297ee56860315a53ec791dd9efcbe55caa93c23
Cristian Montes <cmontesmujica@gmail.com>
2021-09-07 21:54:19 -0700

    adding port

---
+++ controllers/image.js
@@ -0,0 +1,33 @@
+const { json } = require('body-parser');
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey:'58f6b9fdb79340e0afefa0bee880c6f3' // enter YOUR API key here
+  }) ;
+
+  const handleApiCall = (req, res) => {
+      app.models
+      .predict(Clarifai.FACE_DETECT_MODEL , req.body.input)
+      .then(data => {
+          res.json(data);
+      })
+      .catch(err => res.status(400).json('unable to work with API'))
+  }
+
+const handleImage =(req, res, db) => {
+    const {id} = req.body;
+   db('users').where('id', '=', id)
+   .increment('entries', 1)
+   .returning('entries')
+   .then(entries => {
+       res.json(entries[0]);
+   })
+   .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports ={
+    handleImage,
+    handleApiCall
+}
+
+
